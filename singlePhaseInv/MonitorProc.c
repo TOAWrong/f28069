7c3d432001f404ceb24587b65c3e3bff0644fbd5
eunwho <eunwho@gmail.com>
2017-09-16 22:34:16 +0900

    fix sci monitoring

--- singlePhaseInv/MonitorProc.c
+++ singlePhaseInv/MonitorProc.c
@@ -4,7 +4,7 @@
 void monitor_proc()		// need_edit
 {
     static int monitorCount=0;
-    int temp;
+    int i, temp;
     float ftemp;
 	char str[50]= {0};
 
@@ -16,70 +16,35 @@ void monitor_proc()		// need_edit
 	StartTimeMsec = ulGetNow_mSec( );
 
 	if( gMachineState == STATE_POWER_ON ){
-		temp = (int)(floor(Vdc +0.5));
-		snprintf( gStr1,10," VDC =%4d",temp);
-		load_sci_tx_mail_box(gStr1);delay_msecs(20);
-        snprintf( str,19,"No%d adc=%4d",monitorCount,adc_result[monitorCount]);
-        load_scia_tx_mail_box(str); delay_msecs(10);
-        monitorCount++;
-        if(monitorCount>5) monitorCount = 0;
+        load_scia_tx_mail_box(" \r");
+		snprintf( gStr1,10,"Vdc =%.f : ",Vdc);
+		load_sci_tx_mail_box(gStr1);delay_msecs(10);
+		for( i = 0 ; i < 5 ; i++ ){
+		    snprintf( str,19,"No%d=%4d : ",i,adc_result[monitorCount]);
+	        load_scia_tx_mail_box(str);
+		}
 	}
 	else if( gMachineState == STATE_TRIP ){
-        monitorCount ++;
-        if( monitorCount > 6 ) monitorCount = 0;
-	    switch(monitorCount){
-	    case 0 :
-	        load_scia_tx_mail_box("TRIP");
-	        return;
-	    case 1 :
-	        load_scia_tx_mail_box(TripInfoNow.MSG);
-	        return;
-	    case 2 :
-	        sprintf( gStr1,"CODE=%d",TripInfoNow.CODE)    ; load_scia_tx_mail_box(gStr1);
-	        return;
-	    case 3 :
-	        sprintf( gStr1,"DATA=%e",TripInfoNow.DATA)    ; load_scia_tx_mail_box(gStr1);
-	        return;
-	    case 4 :
-	        sprintf( gStr1,"Irms=%e",TripInfoNow.CURRENT) ; load_scia_tx_mail_box(gStr1);
-	        return;
-	    case 5 :
-	        sprintf( gStr1,"RPM =%e",TripInfoNow.RPM)     ; load_scia_tx_mail_box(gStr1);
-	        return;
-	    case 6 :
-	        sprintf( gStr1,"VDC =%e",TripInfoNow.VDC)     ; load_scia_tx_mail_box(gStr1);
-	        return;
-	    default :
-	        monitorCount = 0;
-	        return;
-	    }
+        load_scia_tx_mail_box(" \r");
+        load_scia_tx_mail_box("TRIP");
+        load_scia_tx_mail_box(TripInfoNow.MSG);
+        sprintf( gStr1,"CODE=%d : ",TripInfoNow.CODE)    ; load_scia_tx_mail_box(gStr1);
+        sprintf( gStr1,"DATA=%.2e : ",TripInfoNow.DATA)    ; load_scia_tx_mail_box(gStr1);
+        sprintf( gStr1,"Irms=%.2e : ",TripInfoNow.CURRENT) ; load_scia_tx_mail_box(gStr1);
+        sprintf( gStr1,"RPM =%.2e : ",TripInfoNow.RPM)     ; load_scia_tx_mail_box(gStr1);
+        sprintf( gStr1,"Vdc =%.2e",TripInfoNow.VDC)     ; load_scia_tx_mail_box(gStr1);
     }
     else {
-        if( monitorCount == 0 ){
-            load_scia_tx_mail_box("\r");
-            load_scia_tx_mail_box(MonitorMsg);
-            load_scia_tx_mail_box(" : ");
-       }
-        else if( monitorCount == 1 ){
-            sprintf( gStr1,"Out =%.1f",codeRateHz * reference_out); load_scia_tx_mail_box(gStr1);
-            load_scia_tx_mail_box(" : ");
-        }
-        else if( monitorCount == 2 ){
-            sprintf( gStr1,"Im  =%.1f",rmsIm); load_scia_tx_mail_box(gStr1);
-            load_scia_tx_mail_box(" : ");
-        }
-        else if( monitorCount == 3 ){
-            sprintf( gStr1,"Ia  =%.1f",rmsIa); load_scia_tx_mail_box(gStr1);
-            load_scia_tx_mail_box(" : ");
-        }
-        else if( monitorCount == 4 ){
-            sprintf( gStr1,"Vdc =%.f",lpfVdc); load_scia_tx_mail_box(gStr1);
-        }
-        else{
-            monitorCount = 0;
-        }
-        monitorCount ++;
-        if(monitorCount > 4) monitorCount = 0;
+        load_scia_tx_mail_box("\r");
+        load_scia_tx_mail_box(MonitorMsg);
+        load_scia_tx_mail_box(" : ");
+        sprintf( gStr1,"Out =%.1f",codeRateHz * reference_out); load_scia_tx_mail_box(gStr1);
+        load_scia_tx_mail_box(" : ");
+        sprintf( gStr1,"Im  =%.1f",rmsIm); load_scia_tx_mail_box(gStr1);
+        load_scia_tx_mail_box(" : ");
+        sprintf( gStr1,"Ia  =%.1f",rmsIa); load_scia_tx_mail_box(gStr1);
+        load_scia_tx_mail_box(" : ");
+        sprintf( gStr1,"Vdc =%.f",lpfVdc); load_scia_tx_mail_box(gStr1);
 	}
 }
 
